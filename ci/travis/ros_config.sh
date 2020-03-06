eef119c25d91241dd072b39a7fe7ca1158142eb3
Andreea Sandulescu <andreea.sandulescu@analog.com>
2020-03-06 14:32:41 +0200

    added install ros base package command

--- ci/travis/ros_config.sh
+++ ci/travis/ros_config.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu ${TRAVIS_DIST} main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-if [ "{$ROS_DISTRO}"  = "melodic" ]; then sudo apt update -qq; else sudo apt-get update -qq;  fi
+if [[ "{$ROS_DISTRO}"  = "melodic" ]]
+then 
+  sudo apt update -qq
+  sudo apt install -y python-catkin-pkg ros-$ROS_DISTRO-ros-base
+else
+  sudo apt-get update -qq
+  sudo apt-get install -y python-catkin-pkg ros-$ROS_DISTRO-ros-base
+fi
 source /opt/ros/${ROS_DISTRO}/setup.bash
 sudo rosdep init
 rosdep update
